37c58f89181aedadd7a5e2f8276843dec7844d21
Ēriks Remess <eriks@remess.lv>
2022-08-06 18:21:56 +0300

    cleanup of the code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { Strategy as PassportStrategy } from 'passport';
+import {
+  Strategy as PassportStrategy
+} from 'passport';
 import crypto from 'crypto';
 
 const generateRandomString = (symbols) => {
@@ -15,52 +17,58 @@ export default class Strategy extends PassportStrategy {
   constructor(options, verify) {
     super();
     this.name = 'twitter';
-    this._verify = verify;
-    this._callbackURL = options.callbackURL;
-    this._clientID = options.clientID;
-    this._clientSecret = options.clientSecret;
-    this.scope = [
+    this.scope = options.scope || [
       "tweet.read",
       "offline.access",
       "users.read",
     ];
+    this._callbackURL = options.callbackURL;
+    this._clientID = options.clientID;
+    this._clientSecret = options.clientSecret;
+    this._verify = verify;
   }
   async authenticate(req) {
-    const verified = (error, user, info) => {
-      if (error) return this.error(error);
-      if (!user) return this.fail(info);
-      return this.success(user, info);
-    }
     if (req.query?.code && req.query?.state) {
       try {
-        const { code, state } = req.query;
-        const { code_verifier, state: session_state } = req.session;
-        if (!code_verifier || !state || !session_state || !code) {
+        const {
+          code,
+          state,
+        } = req.query;
+        const {
+          code_verifier,
+          state: session_state
+        } = req.session;
+        if (!code || !code_verifier || !session_state || !state) {
           return this.fail('You denied the app or your session expired!');
         }
         if (state !== session_state) {
           return this.fail('Stored tokens didn\'t match!');
         }
         let body = new URLSearchParams({
-          code,
-          code_verifier,
-          redirect_uri: this._callbackURL,
-          grant_type: 'authorization_code',
           client_id: this._clientID,
           client_secret: this._clientSecret,
+          code,
+          code_verifier,
+          grant_type: 'authorization_code',
+          redirect_uri: this._callbackURL,
         });
         let data = await fetch('https://api.twitter.com/2/oauth2/token', {
           method: 'POST',
-          headers:  {
+          headers: {
             Authorization: `Basic ${Buffer.from(`${encodeURIComponent(this._clientID)}:${encodeURIComponent(this._clientSecret)}`).toString('base64')}`,
           },
           body,
         }).then(res => res.json());
-        const { access_token, refresh_token, expires_in } = data;
+        const {
+          access_token,
+          refresh_token,
+        } = data;
         if (!access_token) {
           return this.fail('No access token!');
         }
-        let { data: profile } = await fetch('https://api.twitter.com/2/users/me', {
+        let {
+          data: profile
+        } = await fetch('https://api.twitter.com/2/users/me', {
           headers: {
             Authorization: `Bearer ${access_token}`,
           }
@@ -68,37 +76,47 @@ export default class Strategy extends PassportStrategy {
         if (!profile) {
           return this.fail('No profile!');
         }
-        return this._verify(access_token, refresh_token, profile, verified);
+        return this._verify(access_token, refresh_token, profile, (error, user, info) => {
+          if (error) return this.error(error);
+          if (!user) return this.fail(info);
+          return this.success(user, info);
+        });
       } catch (error) {
         return this.error(error);
       }
     }
     if (req.query?.error) {
-      return this.fail(req.query.error === "access_denied" ? "You denied the app!" : String(req.query.error));
+      return this.fail(
+        req.query.error === "access_denied"
+         ? "You denied the app!"
+         : String(req.query.error)
+      );
     }
     let state = generateRandomString(32);
     let code_verifier = generateRandomString(128);
     let code_challenge = crypto
-                                  .createHash('sha256')
-                                  .update(code_verifier)
-                                  .digest('base64')
-                                  .replace(/\+/g, '-')
-                                  .replace(/\//g, '_')
-                                  .replace(/\=/g, '');
-    req.session.code_verifier = code_verifier;
+      .createHash('sha256')
+      .update(code_verifier)
+      .digest('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/\=/g, '');
     req.session.code_challenge = code_challenge;
+    req.session.code_verifier = code_verifier;
     req.session.state = state;
     const params = new URLSearchParams({
-      response_type: 'code',
       client_id: this._clientID,
-      redirect_uri: this._callbackURL,
-      state,
       code_challenge,
       code_challenge_method: 'S256',
-      scope: 'tweet.read offline.access users.read',
+      redirect_uri: this._callbackURL,
+      response_type: 'code',
+      scope: this.scope.join(' '),
+      state,
     }).toString();
     return this.redirect(`https://twitter.com/i/oauth2/authorize?${params}`);
   }
 }
 
-export { Strategy };
+export {
+  Strategy
+};
